d76ab3d005a3cfd5dd812d6949d0972e8c2a2cab
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2026-07-03 09:19:00 +0800

    Add CORS to app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from contextlib import asynccontextmanager
+import os
 
 from app.database import init_db, kill_engine
 
 from app.routes import auth, crud
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,6 +16,25 @@ async def lifespan(app: FastAPI):
     await kill_engine()
 
 app = FastAPI(title="Todo", lifespan=lifespan)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+if os.getenv("ALLOWED_ORIGINS"):
+    origins = os.getenv("ALLOWED_ORIGINS").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def get_root():
+    return {"message":"backend up!"}
 
 app.include_router(auth.router)
 app.include_router(crud.router)
